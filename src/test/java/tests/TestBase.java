81eff0f9f7c8cf5aad879d6aef81642ba86ae4c7
zero <viktorkin0511@gmail.com>
2023-12-19 20:24:01 +0300

    Added Jenkins integration

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,8 +3,11 @@ package tests;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.CorporatePage;
 import pages.SmartfonyPage;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Configuration.*;
 
@@ -18,8 +21,19 @@ public class TestBase
     static void setEnvironment()
     {
         baseUrl = "https://www.citilink.ru";
-        browserSize = "1920x1080";
         pageLoadTimeout = 50000;
+        browser = "chrome";
+        browserVersion = "100.0";
+        browserSize = "1920x1080";
+        remote = System.getProperty("remote","https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        browserCapabilities = capabilities;
 
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
